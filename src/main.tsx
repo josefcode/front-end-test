71a0aab5f4cc094236082cb74d4ebdf782d13d14
unknown <atef.chela@gmail.com>
2024-01-26 13:48:28 -0800

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import {
+  createTheme,
+  StyledEngineProvider,
+  ThemeProvider,
+} from '@mui/material/styles'
+import defaultTheme from './theme'
+
+const theme = createTheme(defaultTheme)
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+    <ThemeProvider theme={theme}>
+    <App />
+    </ThemeProvider>
+    </StyledEngineProvider>
+  </React.StrictMode>,
+)
---
+++ src/theme.ts
@@ -0,0 +1,24 @@
+export default {
+    palette: {
+        // primary: {
+        //     main: '',
+        //     dark: '',
+        //     light: '',
+        // },
+        secondary: {
+            main: '#FF6500',
+            dark: 'rgb(66, 70, 77)',
+            light: '#B7BABF',
+            lighter: '#EFECEC',
+          },
+    },
+    breakpoints: {
+        values: {
+          xs: 0,
+          sm: 600,
+          md: 960,
+          lg: 1440,
+          xl: 1920,
+        },
+      },
+}
