8d011dd36f73d2cb41ac2e7e3719970a1e043edb
Atef Chelaghma <81826528+josefcode@users.noreply.github.com>
2024-02-11 21:44:07 +0000

    solve coflict code

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,65 +1,136 @@
+import { useState, ChangeEvent, FormEvent } from "react";
+import { Link } from "react-router-dom";
+import { FormControl, TextField } from "@mui/material";
+import { FormContainer, MainContainer, SectionContainer } from "./LoginStyle";
+import logo from "../../assets/logo.png";
+import { LoginProps, FormErrors, FormData } from "./type";
 
-import { FormControl, TextField } from "@mui/material"
-import { FormContainer, MainContainer, SectionContainer, } from "./LoginStyle"
-import { useState } from "react"
-import { Link } from 'react-router-dom'
-import logo from '../../assets/logo.png'
+const Login = ({ resetPassword }: LoginProps) => {
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState<FormErrors>({
+    email: false,
+    password: false,
+  });
+  const { email, password } = formData;
+  const emailValidationRegex =
+    /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  const emailValid = email.match(emailValidationRegex);
+  const passwordValid = password.length >= 6;
 
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
-const Login = () => {
-    const [error] = useState(false)
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setErrors({ email: !emailValid, password: !passwordValid });
+
+    if (emailValid && passwordValid) {
+      if (email === "test@gmail.com" && password === "test123") {
+        alert("Login successful");
+        setFormData({ email: "", password: "" });
+        setErrors({ email: false, password: false });
+      } else {
+        alert("Login failed. Please use valid credentials.");
+      }
+    }
+  };
+
+  const handleResetPassword = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (emailValid) {
+      alert("A reset link has been sent to your email.");
+      setFormData({ email: "", password: "" });
+      setErrors((prev) => ({ ...prev, email: false }));
+    }
+
+    return setErrors((prev) => ({ ...prev, email: !emailValid }));
+  };
+
   return (
     <MainContainer>
-    <div className="triangle"></div>
-    <SectionContainer>
-   
+      <div className="triangle"></div>
+      <SectionContainer>
         <div className="login__root">
-        <div className="login__logoSide">
+          <div className="login__logoSide">
             <figure>
-                <img src= {logo} alt="" />
+              <img src={logo} alt="Company Logo" />
             </figure>
+            <p>Car Cleaning Company</p>
+            <p>India’s First Waterless</p>
+          </div>
 
-            <p>car cleaning company</p>
-            <p>India’s first waterless</p>
-        </div>
-        
-        <FormContainer>
-        <Link className = "login__helpLink" to = 'help'>need help</Link>
-            <div className = "login__inputContainer">
-            
-                <p className = "login__header">login</p>
-                <FormControl fullWidth sx={{ m: 1 }}>
-                <label className = "login__label">Email</label>
+          <FormContainer
+            onSubmit={resetPassword ? handleResetPassword : handleSubmit}
+          >
+            {resetPassword ? null : (
+              <Link className="login__helpLink" to="help">
+                Need Help
+              </Link>
+            )}
 
-                <TextField 
-                error = {error}
-                id="outlined-basic" 
-                placeholder="joe@email.com"
-                variant="outlined" 
-                helperText={ error ? "Incorrect email." : null}
-                required
+            <div className="login__inputContainer">
+              <p className="login__header">
+                {resetPassword ? "Reset Password" : "Login"}
+              </p>
+              <FormControl fullWidth sx={{ m: 1 }}>
+                <label className="login__label">
+                  {resetPassword ? "Email for password reset" : "Email"}
+                </label>
+                <TextField
+                  error={errors.email}
+                  id="email"
+                  name="email"
+                  placeholder="joe@email.com"
+                  value={email}
+                  variant="outlined"
+                  helperText={errors.email ? "Add valid email." : null}
+                  required
+                  onChange={handleOnChange}
                 />
-                </FormControl>
+              </FormControl>
+              {!resetPassword && (
                 <FormControl fullWidth sx={{ m: 1 }}>
-                <label className = "login__label">Password</label>
-                <TextField 
-                error = {error}
-                id="outlined-basic" 
-                placeholder="password"
-                variant="outlined"
-                helperText={ error ? "Incorrect password." : null}
-                required
-                 />
-                 </FormControl>
-                <Link className = "login__forgetPassword"  to = "forget-password">forget password?</Link>
-                <input className="form__button" type="submit" value = "login"/>
+                  <label className="login__label">Password</label>
+                  <TextField
+                    error={errors.password}
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    variant="outlined"
+                    value={password}
+                    helperText={
+                      errors.password
+                        ? "Password should be at least 6 characters."
+                        : null
+                    }
+                    onChange={handleOnChange}
+                    required
+                  />
+                </FormControl>
+              )}
+              {!resetPassword && (
+                <Link className="login__forgetPassword" to="forget-password">
+                  Forgot Password?
+                </Link>
+              )}
+              <input
+                className="form__button"
+                type="submit"
+                value={resetPassword ? "Reset Password" : "Login"}
+              />
             </div>
-        </FormContainer>
+          </FormContainer>
         </div>
-    </SectionContainer>
+      </SectionContainer>
     </MainContainer>
-  )
-}
+  );
+};
 
-
-export default Login
+export default Login;
